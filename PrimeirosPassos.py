35acbb34ae977451958f01b09c730a5fba8781e4
EuricoJr <ddom1459@gmail.com>
2025-08-18 20:16:58 -0300

    Primeiro dia aprendendo a linguagem

---
+++ PrimeirosPassos.py
@@ -0,0 +1,29 @@
+#print("hello world")
+
+
+#Drawing a Shape (Desenhar um triângulo) * A Ordem dos comandos influenciam
+
+#print("   /|")
+#print("  / |")
+#print(" /  |")
+#print("/___|")
+
+#Variables and Datas Types
+
+
+Character_name = "Jhon" #String = text
+Character_age = "35" #Number
+isMale = False  #boolean value ( represent true or false data)
+print("There once was a man named " + Character_name + ".")
+print("he was "+ Character_age + " years old.")
+
+Character_name = "Mike"
+print("He really like the name " + Character_name + ",")
+print("but didn`t like being "+ Character_age + ".")
+
+#Working with Strings (textos)
+phrase = "Giraffe Academy"
+print(phrase + "is cool")
+print("Giraffe\nAcademy")
+
+
