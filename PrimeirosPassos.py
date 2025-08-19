a78392e1659ec332b6d9230d9ab65b81db16b351
EuricoJr <ddom1459@gmail.com>
2025-08-19 20:55:49 -0300

    Um pouco mais de aprendizado com strins e numbers

--- PrimeirosPassos.py
+++ PrimeirosPassos.py
@@ -11,19 +11,48 @@
 #Variables and Datas Types
 
 
-Character_name = "Jhon" #String = text
-Character_age = "35" #Number
-isMale = False  #boolean value ( represent true or false data)
-print("There once was a man named " + Character_name + ".")
-print("he was "+ Character_age + " years old.")
+#Character_name = "Jhon" #String = text
+#Character_age = "35" #Number
+#isMale = False  #boolean value ( represent true or false data)
+#print("There once was a man named " + Character_name + ".")
+#print("he was "+ Character_age + " years old.")
 
-Character_name = "Mike"
-print("He really like the name " + Character_name + ",")
-print("but didn`t like being "+ Character_age + ".")
+#Character_name = "Mike"
+#print("He really like the name " + Character_name + ",")
+#print("but didn`t like being "+ Character_age + ".")
 
 #Working with Strings (textos)
-phrase = "Giraffe Academy"
-print(phrase + "is cool")
-print("Giraffe\nAcademy")
+#phrase = "Giraffe Academy"
+#print(phrase + "is cool")
+#print("Giraffe\nAcademy")
 
+#phrase = "Giraffe Academy"
+         #0123
+#print(phrase.upper().isupper()) #isupper()..........
+#print(len(phrase))  #Função de comprimento
+#print(phrase[0])
+#print(phrase.index("a"))
+#print(phrase.replace("Giraffe","Elephant"))
+
+#Working with numbers
+#print(3+4) # +,*,/, todas as operações
+#my_num=5
+#print(str(my_num) + " is my favorite number")
+#print(abs(my_num)) #abs da o valor absoluto que esta ali ,
+#print(pow(3,2)) #pow  vai permitir passar 2 tipos de informações, primeiro é o numero e segundo é o poder do numero, como se fosse elevar a algo
+#print(max(4,6))
+#print(round(3.9)) # serve para arredondar o número
+
+#from math import *  #usa para usar outras funções
+#print(floor(4.1)) #arredonda para baixo
+#print(ceil(3.7))
+#print(sqrt(3.1))
+
+#Get input From Users  (permite que um usuário insira informações no programa)
+
+#name = input("Enter your name: ")
+#age = input("Enter your age: ")
+#print("Hello " +name+ "! You are " + age  )
+
+#Building a basic calculator
 
