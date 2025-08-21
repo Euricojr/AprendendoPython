01b32796be3b4a095d5162ab43b51aca384e9e8a
EuricoJr <ddom1459@gmail.com>
2025-08-20 22:00:41 -0300

    Aprendendo sobre uma calculadora e iniciando listas.

--- PrimeirosPassos.py
+++ PrimeirosPassos.py
@@ -56,3 +56,36 @@
 
 #Building a basic calculator
 
+#num1 =  input("enter a number: ")
+#num2 = input("enter another number: ")
+#result=num1+num2
+#print(result)    # torna uma string, dai a soma fica errada, só testar esse código
+
+#num1 =  input("enter a number: ")
+#num2 = input("enter another number: ")
+#result= int(num1)+int(num2) #transforma em número, apenas numeros
+#print(result)    #com número decimal, a função int da problema pois ela só considera número inteiro, para isso é preciso trocar para float
+
+#num1 =  input("enter a number: ")
+#num2 = input("enter another number: ")
+#result= float(num1)+float(num2) #transforma em número, apenas numeros
+#print(result)
+
+
+#Mad libs Game (jogo de palavras)
+
+#color = input("Enter a color: ")
+#plural_noun = input("Enter a plural noun: ")
+#celebrity = input("Enter a celebrity: ")
+
+#print("Roses are "+ color)
+#print(plural_noun +" are blue")
+#print("I love "+ celebrity)
+
+#Lists (datas you need to organize)
+#friends =["Kevin", "Karen", "Jim"] #usa os colchetes para mostrar que é uma lista
+#friends[1] = "Mike"
+#print(friends[1])
+
+#List Functions
+
